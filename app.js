6f3e7e89e3a37ee812f421a46b0433ecee3704e8
Cr-Israel <carlosisrael08@hotmail.com>
2025-01-16 14:23:17 -0300

    Função de sortear amigos

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-
 // Array de nomes
-let names = [];
+let nomes = [];
 
-// Função para adicionar esses nomes
+// Função para adicionar amigos
 function adicionarAmigo() {
   const inputElement = document.getElementById('amigo')
-  const input = inputElement.value
-  input.value == '' ? alert('Por favor, insira um nome.') : names.push(input)
-  
+  const input = inputElement.value.trim()
+  input == '' ? alert('Por favor, insira um nome.') : nomes.push(input)
+
   listaDeAmigos()
-  
+
   inputElement.value = ''
-  console.log(names)
 }
 
+// Função para listar amigos
 function listaDeAmigos() {
   const ul = document.getElementById('listaAmigos')
   ul.innerHTML = ''
-  
-  names.forEach(name => {
+
+  nomes.forEach(name => {
     const li = document.createElement('li')
     li.textContent = name
     ul.appendChild(li)
   });
+}
+
+// Função para sortear amigos
+function sortearAmigo() {
+  
+  if (nomes == [] || nomes.length == 0) {
+    alert('Por favor, adicione um nome antes de sortear')
+  } else {
+    // const amigoSecreto = nomes[Math.floor(Math.random() * nomes.length)]
+    const index = Math.floor(Math.random() * nomes.length)
+    const amigoSecreto = nomes[index]
+
+    const ul = document.getElementById('resultado')
+    ul.innerHTML = ''
+
+    const li = document.createElement('li')
+    li.textContent = `Seu amigo secreto sorteado é: ${amigoSecreto}`
+    ul.appendChild(li)
+  }
 }
